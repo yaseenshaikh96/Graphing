190dcae6f52b4af36210a6866f7f3c819d966f04
yaseen <shaikhyaseen6496@gmail.com>
2022-06-14 21:23:57 +0530

    initial

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.SetWindowSize(200, 50);
+
+        // now can do graph!
+
+        System.Console.Read();
+    }
+}
